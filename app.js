e466ce8d8a585af5b9a65bfd9d10ab7a6b00b5e3
Jiten Mehta <jitenmehta03@gmail.com>
2020-03-23 19:06:13 +0000

    Fundementals done!! Need to look at exceeds grade. early there!

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const startScreenOverlay = document.getElementById('overlay');
 const startScreenOverlayHeading = document.querySelector('#overlay h2');
 const phraseUL = document.querySelector('#phrase ul');
 let letterMissed = 0;
-const playerTry = document.querySelector('#scoreboard ol ');
+const playerTry = document.querySelectorAll('#scoreboard ol li');
 
 
 
 let missed = 0;
 
 const startGameButton = document.querySelector('#overlay a');
+
 
 // array of phrases
 
@@ -22,12 +23,12 @@ const phrases = ['In a pickle',
 'Finger painting  '
 ]
 
+
+
 // Start Game button slide left effect
 
 startGameButton.addEventListener('click', () =>{
-  startScreenOverlay.style.left = '100%';
-  startScreenOverlayHeading.style.opacity = '0';
-  startGameButton.style.opacity = '0';
+  startScreenOverlay.style.display = 'none';
 });
 
 // function that Generats a random phrase and then splits the lettes randomly
@@ -38,22 +39,23 @@ return arr[randomNum].split('');
 }
 
 // function that appends the characters of a phrase as a list item in a UL
-const randomPhrase = getRandomPhraseAsArray(phrases);
+
 const addPhraseToDisplay = (arr) =>{
-  for (let i=0; i < randomPhrase.length; i++){
+  for (let i=0; i < arr.length; i++){
     let li = document.createElement('li');
     li.innerHTML = randomPhrase[i];
     if (li.textContent !== " "){
       li.className = "letter";
     }
     else{
-      li.textContent = ' '
+      li.classList.add('space');
     }
     phraseUL.append(li);
 }
 }
 
 // const randomPhrase = getRandomPhraseAsArray(phrases);
+const randomPhrase = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(randomPhrase);
 
 // Checkletter function
@@ -61,27 +63,52 @@ addPhraseToDisplay(randomPhrase);
 // This function checks if the textContent of the keyboard letters is the same as the letters in the phrase
 
 const checkLetter = (button) =>{
-  let letters = document.getElementsByClassName('letter');
-  for(let i =0; i < letters.length; i++){
-    if (letters[i].textContent.toUpperCase() === button.textContent.toUpperCase() ){
-      letters[i].classList.add('show');
-    } else {
-      letters = null;
+  const letter = document.getElementsByClassName('letter');
+  let matchFound = null;
+  for(let i =0; i < letter.length; i++){
+    if (letter[i].textContent.toUpperCase() === button.textContent.toUpperCase() ){
+      matchFound = letter[i];
+      matchFound.classList.add('show');
     }
   }
-  return letters
+  return matchFound;
 }
 
 
 // Event listener for the display keyboard. This calls the checkletter function
 
-keyboardContainer.addEventListener('click', (e)=>{
-  if (e.target.tagName = 'BUTTON') {
+
+
+const checkWin = () =>{
+  const letter = document.querySelectorAll('.letter');
+  const lishow = document.querySelectorAll('.show');
+  const heading = startScreenOverlay.querySelector('h2');
+
+  if (letter.length === lishow.length){
+    startScreenOverlay.className = 'win';
+    heading.textContent = 'Well Done. You\'ve guessed the correct Phrase';
+    startScreenOverlay.style.display = 'flex';
+
+  } else if (letterMissed >= 5) {
+    startScreenOverlay.className = 'lose';
+    heading.textContent = 'Oh no! You\'ve ran out of geusses. Press the reset button to try again';
+    startScreenOverlay.style.display = 'flex';
+  }
+}
+
+keyboardContainer.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    e.target.classList.add('chosen');
     const letterFound  = checkLetter(e.target);
     if (letterFound === null){
-      playerTry.firstElementChild.remove();
-      letterMissed = +1;
-      console.log(letterMissed);
+      letterMissed += 1;
+      for (i = 4; i >=0; i-= 1){
+        if(playerTry[i].firstElementChild.src.includes('images/liveHeart.png')) {
+                playerTry[i].firstElementChild.src = 'images/lostHeart.png';
+                break;
+            }
+      }
     }
   }
+  checkWin();
 });
